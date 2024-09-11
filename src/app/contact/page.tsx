6a4a6836a2e1566b8ba75c7c8c1780a428882581
Gautam Kumar <g.k965069@gmail.com>
2024-09-11 14:12:15 +0530

    somthing added

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,9 @@
 'use client';
 
 import React, { FormEvent, useState } from 'react';
-import { BackgroundBeams } from '@/components/ui/background-beams';
+// import { BackgroundBeams } from '@/components/ui/background-beams';
+import { BackgroundBeamsWithCollision } from "@/components/ui/background-beams-with-collision";
+ 
 
 function MusicSchoolContactUs() {
   const [email, setEmail] = useState('');
@@ -17,7 +19,7 @@ function MusicSchoolContactUs() {
       {' '}
       {/* Ensure the container is relative */}
       {/* BackgroundBeams with adjusted z-index */}
-      <BackgroundBeams className="absolute top-0 left-0 w-full h-full z-0" />
+      <BackgroundBeamsWithCollision className="absolute top-0 left-0 w-full min-h-full z-0" children={undefined} />
       {/* Content with higher z-index */}
       <div className="max-w-2xl mx-auto p-4 relative z-10">
         {' '}
